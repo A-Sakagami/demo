ba4e26d10bbacf909f1ba80c40a2f1b8ab9cb833
A-Sakagami <akiyasu.sakagami@gmail.com>
2024-04-11 13:56:50 +0900

    Personクラスの配置を変えてエラー表示を無くした

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 /**
  * Hello world!
  */
-public final class App {
+public class App {
     private App() {
     }
 
@@ -13,5 +13,13 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Person p = new Person("graffical");
+        p.setFirstName("Ryohma");
+        p.setLastName("Sakamoto");
+        p.setAge(20);
+        System.out.println("My name is " + p.getLastName() + " " + p.getFirstName()+".");
+        System.out.println("My age is " + p.getAge() + " years old.");
+        
     }
 }
--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -1,19 +1,15 @@
-package com;
-/* 抽象クラス */
-public abstract class Person {
-    
-    /* 抽象メソッド:中身は継承先のクラスで柔軟に設定できるが、必ず定義しないといけない */
-    abstract public String abstractMethod();
+package com.example;
+/* クラス */
+public class Person {
 
     private String firstName;
     private String lastName;
     private int age;
     public String profession;
 
-
-    /* コンストラクタ　*/
-    Person(String firstName, String lastName, int age, String profession){
-      this.profession = profession;
+    /* コンストラクタ */
+    public Person(String profession) {
+        this.profession = profession;
     }
 
     /* その他のメソッドも定義できる。 */
