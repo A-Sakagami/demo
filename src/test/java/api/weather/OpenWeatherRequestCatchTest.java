b9a8d888d3e541246efc81ec5e2a365e4acdf4ba
A-Sakagami <akiyasu.sakagami@gmail.com>
2024-04-16 09:01:01 +0900

    テストコード準備

--- src/test/java/api/weather/OpenWeatherRequestCatchTest.java
+++ src/test/java/api/weather/OpenWeatherRequestCatchTest.java
@@ -1,25 +1,7 @@
 package api.weather;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.mockito.*;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 /* TestInstanceアノテーション：@BeforeAllメソッドが非静的（staticでない）メソッドとして記述できるようになり、インスタンス変数を直接初期化できるようになる */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
